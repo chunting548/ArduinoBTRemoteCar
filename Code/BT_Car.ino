cc6159b38bd8127102d398a1f38a382e37c6bee7
Christine Liu <c6869ppm@gmail.com>
2017-04-21 21:14:29 +0800

    Update BT_Car.ino

--- Code/BT_Car.ino
+++ Code/BT_Car.ino
@@ -7,6 +7,15 @@ int LM_speed = 6; //左輪速度
 int RM_speed = 5; //右輪速度 
 int RM_direction = 7; //右輪方向
 
+//Sensors:
+int D1 = A0;
+int D2 = A1;
+int D3 = A2;
+int D4 = A3;
+int D5 = A4;
+
+int sensorValue = 0; // 儲存感測值
+
 const byte trigPin = 2;
 const byte echoPin = 3;
 
@@ -37,15 +46,19 @@ void setup(){
   pinMode(LM_speed,OUTPUT);
   
   Serial.begin(9600);
+  
   BT.begin(9600);
+  
   Serial.println("Bluetooth is READY!");
   
 }
 
 void stopCar() { // 馬達停止
 
-  analogWrite(RM_speed, fspeed); // 馬達 A 的 PWM 輸出
-  analogWrite(LM_speed, fspeed); // 馬達 B 的 PWM 輸出
+  digitalWrite(RM_direction, LOW);
+  digitalWrite(LM_direction, LOW);
+  analogWrite(RM_speed, cardirection);
+  analogWrite(LM_speed, cardirection);
   
 }
 
@@ -53,8 +66,8 @@ void forward() { //D7 = 1, D8 = 1
   
   digitalWrite(RM_direction, HIGH);
   digitalWrite(LM_direction, HIGH);
-  analogWrite(RM_speed, 200);
-  analogWrite(LM_speed, 200);
+  analogWrite(RM_speed, fspeed);
+  analogWrite(LM_speed, fspeed);
   
 }
 
@@ -62,8 +75,8 @@ void backward() { //D7 = 0, D8 = 0
 
   digitalWrite(RM_direction, LOW);
   digitalWrite(LM_direction, LOW);
-  analogWrite(RM_speed, 200);
-  analogWrite(LM_speed, 200);
+  analogWrite(RM_speed, 255);
+  analogWrite(LM_speed, 255);
 
 }
 
@@ -71,8 +84,8 @@ void turnLeft() { // D7 = 1, D8 = 0
 
   digitalWrite(RM_direction, HIGH);
   digitalWrite(LM_direction, LOW);
-  analogWrite(RM_speed, 200);
-  analogWrite(LM_speed, 200);
+  analogWrite(RM_speed, fspeed);
+  analogWrite(LM_speed, fspeed);
   
 }
 
@@ -80,8 +93,8 @@ void turnRight() { //D7 = 0, D8 = 1
 
   digitalWrite(RM_direction, LOW);
   digitalWrite(LM_direction, HIGH);
-  analogWrite(RM_speed, 200);
-  analogWrite(LM_speed, 200);
+  analogWrite(RM_speed, fspeed);
+  analogWrite(LM_speed, fspeed);
   
 }
 
@@ -94,7 +107,7 @@ void loop(){
   Serial.println();
   delay(1000);
   
-  if(distance <= 15){
+  if(distance <= 20){
     stopCar();
     Serial.print("Danger!! Stop the Car!\n");
   }
@@ -102,30 +115,68 @@ void loop(){
   if( BT.available() > 0) {
     
     var = BT.read();
-    
+
     switch(var) {
-      case 102: //F, 0
-        forward();
+      case 102: //f
+       
+        forward(); 
+             
       break;
       
-      case 98: //B, 1
+      case 98: //b
+      
         backward();
+ 
       break;
       
-      case 115: //S, 2
+      case 115: //s
+      
         stopCar();
+        
       break;
       
-      case 114: //R, 3
+      case 114: //r
+      
         turnRight();
+    
       break;
       
-      case 108:
-        turnLeft(); //L, 4
+      case 108: //l
+      
+        turnLeft();
+
       break;
       
       default:
+      
       break;
+      
     }
-  }
+  } 
+
+  //-------Line Follow--------
+  Serial.println("-------------");
+  
+  sensorValue = analogRead(D1); 
+  Serial.print("D1 = "); 
+  Serial.println(sensorValue, OCT); 
+  
+  sensorValue = analogRead(D2); 
+  Serial.print("D2 = "); 
+  Serial.println(sensorValue, OCT); 
+  
+  sensorValue = analogRead(D3); 
+  Serial.print("D3 = "); 
+  Serial.println(sensorValue, OCT); 
+  
+  sensorValue = analogRead(D4); 
+  Serial.print("D4 = "); 
+  Serial.println(sensorValue, OCT);
+   
+  sensorValue = analogRead(D5); 
+  Serial.print("D5 = "); 
+  Serial.println(sensorValue, OCT); 
+  
+  Serial.println("-------------"); 
+  
 }
